0076c29fd139970795ee66226ed51ec580b1e0bd
hxtmike <hxtmike@gmail.com>
2022-07-25 22:39:09 +0800

    some python packages installed

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 import os
 import json
 from flask import Flask, redirect, render_template, request
+from flask_session import Session
+from cs50 import SQL
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
 from svProjectFunctions import getResultForWeb
+from userFunctions import error
 
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///database.db")
+
 # Make sure API key is set
 if not os.environ.get("Google_API_KEY"):
     raise RuntimeError("Google_API_KEY not set")
+gglapikey = os.environ.get("Google_API_KEY")
 
 # open state data json in static file
 with open('./static/states.json', 'r') as stateDataFile:
@@ -18,7 +33,6 @@ with open('./static/states.json', 'r') as stateDataFile:
 
 # print(stateDataStr)
 
-gglapikey = os.environ.get("Google_API_KEY")
 
 @app.route("/")
 def index():
@@ -44,3 +58,8 @@ def stateslist():
         statesDataStr = statesDataFile.read()
     stateslist = json.loads(statesDataStr)
     return render_template("stateslist.html", stateslist = stateslist)
+
+# @app.route('/error')
+# def errorPage():
+#     return error('test msg')
+
---
+++ userFunctions.py
@@ -0,0 +1,20 @@
+from functools import wraps
+
+from flask import render_template, redirect, session
+
+def error(message):
+    print(message)
+    return render_template('error.html', message = message)
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
